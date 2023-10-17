0cdb8fdd7174c4cc0d04bc48d842a981ac9c27ba
juliusbutkus <j.butkus.skouds@gmail.com>
2023-10-17 09:51:23 +0300

    veikiantis adminas

--- balti_zirgai/alynas/admin.py
+++ balti_zirgai/alynas/admin.py
@@ -1,21 +1,21 @@
 from django.contrib import admin
 from . import models
 
-from . import models
 
 class TypeAdmin(admin.ModelAdmin):
-    list_display = ("name", "detail")
-    list_filter = ("name")
+    list_display = ("name", "detail",)
+    list_filter = ("name",)
     
 
 class BeerAdmin(admin.ModelAdmin):
-    list_display = ("beer_type", "name", "qty", "price", "detail")
-    list_filter = ("beer_type", "name")
+    list_display = ("beer_type", "name", "qty", "price", "detail",)
+    list_filter = ("beer_type", "name",)
 
 
 class OrderAdmin(admin.ModelAdmin):
-    list_display = ("order", "quantity", "price", "status", "date")
-    list_filter = ("status", "quantity", "price",) 
+    list_display = ("qty", "status", "date",)
+    list_filter = ("status", "qty",) 
+
 
 admin.site.register(models.Type, TypeAdmin)
 admin.site.register(models.Beer, BeerAdmin)
