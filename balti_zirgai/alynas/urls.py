19bd4634b02a035ad4be2da96bc257bd9b318a94
juliusbutkus <j.butkus.skouds@gmail.com>
2023-10-17 11:41:16 +0300

    beer templates and views

--- balti_zirgai/alynas/urls.py
+++ balti_zirgai/alynas/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path("beer/", views.BeerMeniu, name="beer_meniu" ),
+    path("beer/<int:pk>/", views.BeerDetail, name="beer_detail")
 ]
