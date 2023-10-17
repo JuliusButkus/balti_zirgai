ed7103c2027eef5feedab6e8bb8d4a01db368b78
juliusbutkus <j.butkus.skouds@gmail.com>
2023-10-17 09:20:59 +0300

    admin padarytas

--- balti_zirgai/alynas/admin.py
+++ balti_zirgai/alynas/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
 from . import models
 
+from . import models
+
+class TypeAdmin(admin.ModelAdmin):
+    list_display = ("name", "detail")
+    list_filter = ("name")
+    
+
+class BeerAdmin(admin.ModelAdmin):
+    list_display = ("beer_type", "name", "qty", "price", "detail")
+    list_filter = ("beer_type", "name")
+
+
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ("order", "quantity", "price", "status", "date")
+    list_filter = ("status", "quantity", "price",) 
+
+admin.site.register(models.Type, TypeAdmin)
+admin.site.register(models.Beer, BeerAdmin)
+admin.site.register(models.Order, OrderAdmin)
