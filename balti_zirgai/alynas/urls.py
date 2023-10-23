e0aebe88b4dc6da5e28ae1f717b00cff36f9e0c1
juliusbutkus <j.butkus.skouds@gmail.com>
2023-10-23 08:42:03 +0300

    mini korekcijos

--- balti_zirgai/alynas/urls.py
+++ balti_zirgai/alynas/urls.py
@@ -4,7 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('light-beer/', views.LightBeer.as_view(), name='light_beer'),
-    path('dark-beer/', views.dark_beer, name='dark_beer'),
+    path('dark-beer/', views.DarkBeer.as_view(), name='dark_beer'),
     path("beer/", views.BeerMeniu.as_view(), name="beer_meniu" ),
     path("beer/<str:beer_name>/", views.BeerDetail.as_view(), name="beer_detail"),
     path('buy-beer/<int:beer_id>/', views.buy_beer, name='buy_beer'),
